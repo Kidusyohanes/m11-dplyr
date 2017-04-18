96c5582d3cf7e2d591ad4aa0480c1dc24ff2fd88
Kidusyohanes <kidussendeke@gmail.com>
2017-04-18 13:51:51 -0700

    Made changes

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -18,7 +18,7 @@ vehicles.1997 <- vehicles[vehicles$year == 1997,]
 View(vehicles.1997)
 
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
-unique(vehicles.1997$year == 1)
+unique(vehicles.1997$year == 1997)
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
 two.wheel <- vehicles[vehicles$drive == '2-Wheel Drive' & vehicles$cty > 20,]
